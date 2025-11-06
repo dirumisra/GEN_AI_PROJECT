b3b35805b5097ca844d2a0935f002746e3b1722b
dirumisra <dhirajk266@gmail.com>
2025-11-07 01:40:29 +0530

    pandas foundation l4

---
+++ py_notebooks/genai_notebooks/streamlit/app.py
@@ -0,0 +1,7 @@
+# Minimal “Hello Streamlit” (app.py)
+import streamlit as st
+
+st.set_page_config(page_title="TechConvos-Streamlit Starter",page_icon="🤖",layout = "centered")
+st.title("🤖 TechConvos – Streamlit Starter")
+st.success("Streamlit is running! Now let's add a chat UI + OpenAI.")
+
